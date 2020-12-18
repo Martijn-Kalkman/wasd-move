db902f3c907bb0e23904383100b43ba239033a84
Peter <peter.kalkman@partners4it.nl>
2020-12-18 13:04:50 +0100

    bewegen kan nu met een plaatje

--- wasd.js
+++ wasd.js
@@ -1,32 +1,16 @@
+"use strict";
 
+let canvasWidth = 1500;
+let canvasHeight = 1000;
+let sourceX = 520;
+let sourceY = 120;
+let marioWidth = 18;
+let marioHeight = 18;
 
+let xpos = 50;
+let ypos = 50;
 
-var canvas = document.querySelector('#canvas');
-var context = canvas.getContext ('2d');
-
-var xpos = 0;
-var ypos = 0;
-
-
-context.rect(xpos, ypos, 50, 50);
-context.stroke();
 function move(e) {
-
-    let sourceX = 520;
-    let sourceY = 120;
-    let marioWidth = 18;
-    let marioHeight = 18;
-
-    let marioX = 10;
-    let marioY = 50;
-
-    let marioSpeedX = 10;
-    let marioSpeedY = 0;
-
-
-    //alert(e.keyCode);
-
-
 
     document.onkeydown = move;
 
@@ -44,10 +28,6 @@ function move(e) {
     if(e.keyCode==87){
         ypos-=5;
     }
-
-    canvas.width=canvas.width;
-    context.rect(xpos, ypos, 50, 50);
-    context.stroke();
 }
 
 document.onkeydown = move;
@@ -57,28 +37,28 @@ window.onload = function() {
     let sheet = new Image();   // maak nieuw img element
 
     let canvas = document.getElementById("canvas");
-    let ctx = canvas.getContext("2d");
-    ctx.imageSmoothingEnabled = false; // voorkom antialias effect
+    let context = canvas.getContext("2d");
+    context.imageSmoothingEnabled = false; // voorkom antialias effect
 
     var gameLoop = function(){
         // maak canvas leeg
-
-
-
-
-
-
-
+        context.fillStyle = "#9999ee";
+        context.fillRect(0, 0, canvasWidth, canvasHeight);
 
         // teken
-        ctx.drawImage(sheet, sourceX, sourceY, marioWidth, marioHeight, marioX, marioY, 64, 64);
+        context.drawImage(sheet, sourceX, sourceY, marioWidth, marioHeight, xpos, ypos, 64, 64);
 
-
+        requestAnimationFrame(gameLoop);
     }
+
+    // als spritesheet is geladen, start dan gameloop
+    sheet.addEventListener('load', function(){
+        requestAnimationFrame(gameLoop);
+    }, false);
 
     // als spritesheet is geladen, start dan gameloope);
     // zet bronbestand van plaatje. De browser begint na dit commando
     // naar het plaatje te zoeken en zal proberen het te laden. als dat gelukt is
     // wordt de event handler op regel 45 aangeroepen
-    sheet.src = 'img/sheet.png';
+    sheet.src = 'sheet.png';
 };
